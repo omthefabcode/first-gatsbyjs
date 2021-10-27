2bb60019d66740b74c787b3e4dd68000d6da74d7
omthefabcode <omthefabcode@outlook.com>
2021-10-27 17:12:21 +0530

    image upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import * as React from "react";
 import Layout from "./components/layout";
+// import { StaticImage } from "gatsby-plugin-image";
+
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
+    console.log('console: ', StaticImage);
     return (
         <Layout pageTitle="Home Page">
             <p>This text is comming from Home Page.</p>
+            <StaticImage 
+                alt="Clifford"
+                src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg"
+            />
         </Layout>
     );
 }
